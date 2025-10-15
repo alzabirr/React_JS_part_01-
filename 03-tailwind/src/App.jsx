ce5cafe288be77b94feb14a262ba9cbe65f5e0c4
alzabirr <alzabirhussain@gmail.com>
2025-10-16 03:13:57 +0600

    tailwind css

---
+++ 03-tailwind/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Tailwind_test from './Tailwind_test';
+
+const App = () => {
+  return (
+    <div className='h-screen w-full bg-gray-900'>
+      <Tailwind_test/>
+    </div>
+  );
+};
+
+export default App;
