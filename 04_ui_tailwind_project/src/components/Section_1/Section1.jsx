ae423ec490fc0375d9ef161facc019ac2288d16e
alzabirr <alzabirhussain@gmail.com>
2025-10-21 23:59:46 +0600

    tailwind project

---
+++ 04_ui_tailwind_project/src/components/Section_1/Section1.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const Section1 = () => {
+  return (
+    <div className='h-screen w-full bg-purple-400'>
+      <Navbar/>
+      
+    </div>
+  )
+}
+
+export default Section1
